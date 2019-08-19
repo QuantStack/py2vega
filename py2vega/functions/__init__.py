476a38cc4a33708992009e8236e23b8afd02ceed
martinRenou <martin.renou@gmail.com>
2019-08-19 17:29:26 +0200

    Add string functions

--- py2vega/functions/__init__.py
+++ py2vega/functions/__init__.py
@@ -4,5 +4,9 @@ from .type_checking import type_checking_functions
 from .type_coercing import type_coercing_functions
 from .date_time import date_time_functions
 from .array import array_functions
+from .string import string_functions
 
-vega_functions = math_functions + type_checking_functions + type_coercing_functions + date_time_functions + array_functions
+vega_functions = (
+    math_functions + type_checking_functions + type_coercing_functions +
+    date_time_functions + array_functions + string_functions
+)
