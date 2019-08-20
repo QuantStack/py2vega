dd9e0ed8eb03d033d9ef825e7d87483dee613420
martinRenou <martin.renou@gmail.com>
2019-08-20 09:07:35 +0200

    Add regexp functions

--- py2vega/functions/__init__.py
+++ py2vega/functions/__init__.py
@@ -6,8 +6,10 @@ from .date_time import date_time_functions
 from .array import array_functions
 from .string import string_functions
 from .formatting import formatting_functions
+from .regexp import regexp_functions
 
 vega_functions = (
     math_functions + type_checking_functions + type_coercing_functions +
-    date_time_functions + array_functions + string_functions + formatting_functions
+    date_time_functions + array_functions + string_functions + formatting_functions +
+    regexp_functions
 )
