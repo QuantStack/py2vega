0fe44e7f23564c9d2d330f279090987e787180db
martinRenou <martin.renou@gmail.com>
2019-08-20 09:58:23 +0200

    Add scale functions

--- py2vega/functions/__init__.py
+++ py2vega/functions/__init__.py
@@ -9,9 +9,10 @@ from .formatting import formatting_functions
 from .regexp import regexp_functions
 from .color import color_functions
 from .object import object_functions
+from .scale import scale_functions
 
 vega_functions = (
     math_functions + type_checking_functions + type_coercing_functions +
     date_time_functions + array_functions + string_functions + formatting_functions +
-    regexp_functions + color_functions + object_functions
+    regexp_functions + color_functions + object_functions + scale_functions
 )
